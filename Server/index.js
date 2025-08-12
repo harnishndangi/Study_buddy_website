060c55f1511bad87db8153155c2e558c6657088f
harnishdangi <harnishdangi@gmail.com>
2025-08-12 21:38:19 +0530

    m

--- Server/index.js
+++ Server/index.js
@@ -9,12 +9,16 @@ import pomodoroRoutes from './routes/pomodoros.js';
 import calendarRoutes from './routes/calendar.js';
 
 const app = express();
-
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' })); // Increase limit for larger payloads
 
+
+// app.get("/",(req,res)=>{
+//     res.send("hello")
+// })
 
 
 app.use("/api/auth", authRoutes);
@@ -24,5 +28,8 @@ app.use("/api/pomodoros", pomodoroRoutes);
 app.use("/api/calendar",calendarRoutes);
 
 
-
+app.listen(port,async ()=>{
+    await connectDB();
+    console.log(`Server is running on port http://localhost:${port}`)
+});
 
