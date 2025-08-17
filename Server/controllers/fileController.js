1bacdcd9275c732dca6d19a9116265eb57810395
harnishdangi <harnishdangi@gmail.com>
2025-08-17 11:14:21 +0530

    deleted files

--- Server/controllers/fileController.js
+++
@@ -1,9 +0,0 @@
-// File Controller: Handles media/file uploads to Cloudinary
-
-/**
- * Upload a file to Cloudinary
- */
-export const uploadFile = async (req, res) => {
-  // Implementation here
-  // Use Cloudinary SDK to upload file and return the URL
-};
